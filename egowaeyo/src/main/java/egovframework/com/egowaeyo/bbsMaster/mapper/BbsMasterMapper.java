42b79967e3b0098cddd58c2facd5beee7fd2203d
Lee min young <lmy1147@naver.com>
2025-06-30 17:41:47 +0900

    이민영/게시판추가 모달창 기능구현중/0630

--- egowaeyo/src/main/java/egovframework/com/egowaeyo/bbsMaster/mapper/BbsMasterMapper.java
+++ egowaeyo/src/main/java/egovframework/com/egowaeyo/bbsMaster/mapper/BbsMasterMapper.java
@@ -1,13 +1,23 @@
 package egovframework.com.egowaeyo.bbsMaster.mapper;
 
 import java.util.List;
+import java.util.Map;
 
-import egovframework.com.egowaeyo.article.VO.BoardMasterVO;
+import egovframework.com.egowaeyo.bbsMaster.VO.BoardMasterVO;
 
 public interface BbsMasterMapper {
-	List<BoardMasterVO> slectComtccmmn();
-	List<BoardMasterVO> selectBbsMasterList();
-	public int insertBBSMasterInf(BoardMasterVO boardMaster);
-	public int insertBBSMasterAuth(BoardMasterVO auth);
-	
-	}
+   List<Map<String, String>> selectCombbs();
+   
+   List<BoardMasterVO> selectDeptEmp();
+
+   List<Map<String, String>> selectGroupedBbsData();
+
+   public int insertBBSMaster(BoardMasterVO boardMaster);
+
+   public int insertBBSMasterAuth(BoardMasterVO auth);
+
+   void insertCommonDetailCode(String bbsTyCode, String bbsName, String useAt);
+
+   String getMaxBbsTyCode();
+
+}
--- egowaeyo/src/main/java/egovframework/com/egowaeyo/bbsMaster/service/BbsMasterService.java
+++ egowaeyo/src/main/java/egovframework/com/egowaeyo/bbsMaster/service/BbsMasterService.java
@@ -1,12 +1,24 @@
 package egovframework.com.egowaeyo.bbsMaster.service;
 
 import java.util.List;
+import java.util.Map;
 
-import egovframework.com.egowaeyo.article.VO.BoardMasterVO;
+import egovframework.com.egowaeyo.bbsMaster.VO.BoardMasterVO;
 
 public interface BbsMasterService {
-	List<BoardMasterVO> slectComtccmmn();
-	List<BoardMasterVO> selectBbsMasterList();
-	public int insertBBSMasterInf(BoardMasterVO boardMaster);
+	
+	String getMaxBbsTyCode();
+
+	void insertCommonDetailCode(String bbsTyCode, String bbsName, String useAt);
+
+	public int insertBBSMaster(BoardMasterVO boardMaster);
+
 	public int insertBBSMasterAuth(BoardMasterVO auth);
+	
+	// 모달
+	List<Map<String, String>> getCombbs();
+	List<BoardMasterVO> getDeptEmp();
+
+	// 사이드바
+	Map<String, List<String>> getGroupedBbsData();
 }
