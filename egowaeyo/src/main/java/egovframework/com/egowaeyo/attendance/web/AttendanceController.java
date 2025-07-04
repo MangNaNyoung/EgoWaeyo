53010f654087536b415b1a5a21f6413cdc472dc8
Archi <dnrdl6817@gmail.com>
2025-07-04 17:41:44 +0900

    임성욱/출퇴근 기록 불러오기/0704

--- egowaeyo/src/main/java/egovframework/com/egowaeyo/attendance/web/AttendanceController.java
+++ egowaeyo/src/main/java/egovframework/com/egowaeyo/attendance/web/AttendanceController.java
@@ -1,16 +1,20 @@
 package egovframework.com.egowaeyo.attendance.web;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import egovframework.com.egowaeyo.attendance.service.AttendanceService;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Controller
 @RequestMapping("/attendance")
 public class AttendanceController {
@@ -22,9 +26,15 @@ public class AttendanceController {
 		return "attendance/attendance.html";
 	}
 	
-	@GetMapping("/attendList.do")
+	@PostMapping("/attendList.do")
 	@ResponseBody
-	public List<AttendVO>getAttend(@RequestBody AttendVO vo){
+	public List<AttendVO>getAttend(@RequestBody AttendVO vo, Principal principal){
+		
+		if(vo.getStartDate() == null) {
+			vo.setStartDate(null);
+		}
+		
+		vo.setEmplyrId(principal.getName());
 		
 		return AttendService.getAttend(vo);
 	}
