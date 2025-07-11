1a7ec2bd30462bb28adfd4212069c8083589a66f
Lee min young <lmy1147@naver.com>
2025-07-11 14:02:59 +0900

    이민영/파일업뎃ing/0711

--- egowaeyo/src/main/java/egovframework/com/egowaeyo/article/service/ArticleService.java
+++ egowaeyo/src/main/java/egovframework/com/egowaeyo/article/service/ArticleService.java
@@ -3,7 +3,6 @@ package egovframework.com.egowaeyo.article.service;
 import java.util.List;
 import java.util.Map;
 
-import egovframework.com.cop.bbs.service.Board;
 import egovframework.com.egowaeyo.article.VO.BoardVO;
 
 public interface ArticleService {
@@ -21,9 +20,9 @@ public interface ArticleService {
 	int deleteArticle(BoardVO boardVO);
 
 	List<Map<String, String>> getBbsFilter();
-	
+
 	List<Map<String, Object>> filterArticles(Map<String, Object> params);
-	
-	int updateArticleRdcnt(BoardVO boardVO); //조회수
+
+	int updateArticleRdcnt(BoardVO boardVO); // 조회수
 
 }
